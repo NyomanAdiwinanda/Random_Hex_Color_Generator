b41c23049a97916c75fc8426bf0f7285be397a91
Nyoman Adiwinanda <nyoman.adi16@gmail.com>
2020-12-18 12:47:03 +0700

    1) adding HTML, CSS, JS code 2) adding README.md file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Random Color Generator
+
+A simple project using only plain HTML, CSS, and Javascript to generate new random color with the hex code written on screen 
---
+++ script.js
@@ -0,0 +1,18 @@
+function randomColor() {
+    let output = "#"
+    let value = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"];
+
+    for (i = 0; i < 6; i++) {
+        output += value[ Math.floor( Math.random() * value.length ) ]
+    }
+
+    return output
+}
+  
+document.addEventListener('click', function(event) {
+    if (event.target.className === "generate") {
+    document.querySelector(".remove").style.visibility = "hidden"
+    document.querySelector(".color").innerText = `Hex Code: ${randomColor()}`
+    document.querySelector("body").style.backgroundColor = randomColor()
+    }
+})
